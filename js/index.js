db4b8ba427c455545613b71050417ad52582419b
darkMagic Code <111946821+darkMagicCode@users.noreply.github.com>
2022-09-15 12:14:17 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text-typed', {
+    strings: ['ayman nawwar. ^500 ', 'frontend developer ^500 ', ' freelancer  ^500 ', 'hungry mind !'],
+    loop: true,
+    smartBackspace: true,
+    typeSpeed: 40,
+    backSpeed: 30
+    // smartBackspace: true
+      });
